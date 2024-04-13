e35a7126ca44962fe971ffb2cfa9ba8938aee08b
Velin Arbov <Velin.Arbov@iris.co.uk>
2024-04-13 15:47:11 +0300

    Add project files.

---
+++ RangeManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using RangeManagementSystem.Models;
+using System.Diagnostics;
+
+namespace RangeManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RangeManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RangeManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
